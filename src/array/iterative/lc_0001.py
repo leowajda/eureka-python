c4bf612ab5e5db543930451db6972adeacc2b6d5
Leonardo Wajda <dev.leowajda@tuta.io>
2022-09-24 17:03:03 +0200

    :sparkles: Add iterative solution for Two Sum

---
+++ src/array/iterative/lc_0001.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        map = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in map:
+                return [i, map[complement]]
+
+            map[nums[i]] = i
