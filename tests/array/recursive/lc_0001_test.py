9794b3e94f4d6145517298b1ee377d0f485571c9
Leonardo Wajda <dev.leowajda@tuta.io>
2022-09-24 17:06:15 +0200

    :white_check_mark: Add test for recursive Two Sum

---
+++ tests/array/recursive/lc_0001_test.py
@@ -0,0 +1,19 @@
+from src.array.recursive.lc_0001 import Solution
+from hamcrest import contains_inanyorder, assert_that
+import pytest
+
+
+class TestSolution:
+
+    @pytest.fixture
+    def solution(self):
+        return Solution()
+
+    @pytest.mark.parametrize("nums, target, expected", [
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [0, 2]),
+        ([3, 3], 6, [0, 1])
+    ])
+    def test_two_sum(self, nums, target, expected, solution):
+        result = solution.twoSum(nums, target)
+        assert_that(*result, contains_inanyorder(*expected))
