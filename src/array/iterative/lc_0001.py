7a55b3b8892656bfac98cfbef53d5b0eeee8ea50
Leonardo Wajda <dev.leowajda@tuta.io>
2022-09-24 17:07:54 +0200

    :art: Change indentation

--- src/array/iterative/lc_0001.py
+++ src/array/iterative/lc_0001.py
@@ -8,8 +8,6 @@ class Solution:
 
         for i in range(len(nums)):
             complement = target - nums[i]
-
             if complement in map:
                 return [i, map[complement]]
-
             map[nums[i]] = i
