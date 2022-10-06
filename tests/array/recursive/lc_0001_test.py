e2bc01130fc1deaa652e35efe792e1a14e3d8a52
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-06 20:29:44 +0200

    :art: Change formatting

--- tests/array/recursive/lc_0001_test.py
+++ tests/array/recursive/lc_0001_test.py
@@ -11,8 +11,8 @@ class TestSolution:
 
     @pytest.mark.parametrize("nums, target, expected", [
         ([2, 7, 11, 15], 9, [0, 1]),
-        ([3, 2, 4], 6, [0, 2]),
-        ([3, 3], 6, [0, 1])
+        ([3, 2, 4],      6, [0, 2]),
+        ([3, 3],         6, [0, 1])
     ])
     def test_two_sum(self, nums, target, expected, solution):
         result = solution.twoSum(nums, target)
