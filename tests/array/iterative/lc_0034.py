6662b36d12c29b4b645f32d00bd5f8c0cddd2d60
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-14 12:31:30 +0200

    :white_check_mark: Add test for iterative 'Find First and Last Position of Element in Sorted Array'

---
+++ tests/array/iterative/lc_0034.py
@@ -0,0 +1,20 @@
+import pytest
+from hamcrest import assert_that, equal_to
+from src.array.iterative.lc_0034 import Solution
+
+
+class TestSolution:
+
+    @pytest.fixture
+    def solution(self):
+        return Solution()
+
+    @pytest.mark.parametrize("nums, target, expected", [
+        ([5, 7, 7, 8, 8, 10], 8, [3, 4]),
+        ([5, 7, 7, 8, 8, 10], 6, [-1, -1]),
+        ([], 0, [-1, -1])
+
+    ])
+    def test_search_range(self, solution, nums, target, expected):
+        result = solution.searchRange(nums, target)
+        assert_that(result, equal_to(expected))
