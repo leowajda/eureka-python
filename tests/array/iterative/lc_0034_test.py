248c101e8e09721a119c78b8d1a1fe24b02dd292
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-17 06:21:35 +0200

    :art: Change indentation

--- tests/array/iterative/lc_0034_test.py
+++ tests/array/iterative/lc_0034_test.py
@@ -12,7 +12,7 @@ class TestSolution:
     @pytest.mark.parametrize("nums, target, expected", [
         ([5, 7, 7, 8, 8, 10], 8, [3, 4]),
         ([5, 7, 7, 8, 8, 10], 6, [-1, -1]),
-        ([], 0, [-1, -1])
+        ([],                  0, [-1, -1])
 
     ])
     def test_search_range(self, solution, nums, target, expected):
