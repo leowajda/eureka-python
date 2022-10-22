b77112d8d636b9d3b3c5506fb18de08591aa1818
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-22 14:09:22 +0200

    :white_check_mark: Add test for recursive 'Maximum Depth of Binary Tree'

---
+++ tests/binary_tree/recursive/lc_0104_test.py
@@ -0,0 +1,20 @@
+import pytest
+from hamcrest import assert_that, equal_to
+
+from binary_tree.TreeNode import deserialize
+from src.binary_tree.recursive.lc_0104 import Solution
+
+
+class TestSolution:
+
+    @pytest.fixture
+    def solution(self):
+        return Solution()
+
+    @pytest.mark.parametrize("root, expected", [
+        (deserialize([3, 9, 20, None, None, 15, 7]), 3),
+        (deserialize([1, None, 2]),                  2)
+    ])
+    def test_search(self, solution, root, expected):
+        result = solution.maxDepth(root)
+        assert_that(result, equal_to(expected))
