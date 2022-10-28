d9013a37f973985456e839808f229b7526ddc4e8
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-28 08:58:25 +0200

    :sparkles: Add recursive solution for 'Balanced Binary Tree'

---
+++ src/binary_tree/recursive/lc_0110.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from binary_tree.TreeNode import TreeNode
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        def helper(node: Optional[TreeNode]) -> int:
+
+            if node is None:
+                return 0
+
+            left, right = helper(node.left), helper(node.right)
+            if left is -1 or right is -1 or abs(left - right) > 1:
+                return -1
+
+            return max(left, right) + 1
+
+        return helper(root) is not -1
