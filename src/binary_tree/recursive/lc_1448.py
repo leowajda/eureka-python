a8c86ceb05c4b1fc82842afc1efe20e892515387
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-28 09:01:59 +0200

    :sparkles: Add recursive solution for 'Count Good Nodes in Binary Tree'

---
+++ src/binary_tree/recursive/lc_1448.py
@@ -0,0 +1,12 @@
+from binary_tree.TreeNode import TreeNode
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def helper(node: TreeNode, prev: int) -> int:
+            if node is None:
+                return 0
+            return (node.val >= prev) + helper(node.left, max(node.val, prev)) + helper(node.right, max(node.val, prev))
+
+        return helper(root, root.val)
