e93f5332d94a572445fa868d2ba9ddbf0777c30b
Leonardo Wajda <dev.leowajda@tuta.io>
2022-11-13 14:21:25 +0100

    :zap: Resolve conflicts with pandas API

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -4,6 +4,10 @@ import sys
 import re
 
 
+def fetch_id(path):
+    return int(re.search(r"\d+", path)[0])
+
+
 def fetch_leetcode_url(path):
     name = fetch_problem_name(path)
     url_name = re.sub(r"[^a-zA-Z0-9 ]", "", name).replace(" ", "-").lower()
@@ -21,48 +25,26 @@ def fetch_problem_name(path):
     return re.search(r"\'([^']*)\'", result.stdout)[1]
 
 
-def fetch_id(path):
-    return int(re.search(r"\d+", path)[0])
+prev_frames = pd.read_csv('./docs/data.csv')
+id_col, name_col, lang_col = prev_frames.columns
+n, repo_name = len(sys.argv), sys.argv[1]
 
+prev_frames = prev_frames.assign(lang_col=prev_frames[lang_col].str.split(' ')).explode(lang_col)
+new_frames = [pd.DataFrame(
+    [[fetch_id(sys.argv[i]), fetch_leetcode_url(sys.argv[i]), fetch_github_url(repo_name, sys.argv[i])]],
+    columns=[id_col, name_col, lang_col]) for i in range(2, n)]
 
-def update_files(data):
+frames = pd.concat(new_frames + [prev_frames], ignore_index=True)
+frames = frames.groupby(by=[id_col, name_col], as_index=False)[lang_col].agg(lambda x: set(x))
+frames = frames.set_index(keys=[id_col]).sort_index()
+frames[lang_col] = frames[lang_col].apply(' '.join)
+
+if not frames.equals(prev_frames):
     with open('./docs/data.csv', 'w') as f:
-        f.write(data.to_csv())
+        f.write(frames.to_csv())
 
     with open('./README.md', 'w') as f:
         with open('./docs/content.md', 'r') as s:
             content = s.read()
         f.write(content)
-        f.write(data.to_markdown(colalign=("center", "center", "center")))
-
-
-def populate_data(data):
-    accumulator = [data]
-    repository_name = sys.argv[1]
-
-    n = len(sys.argv)
-    for i in range(2, n):
-
-        file_path = sys.argv[i]
-        id, github_url = fetch_id(file_path), fetch_github_url(repository_name, file_path)
-
-        if id in data.index:
-            prev_record = data.at[id, lang_col]
-            if file_path not in prev_record:
-                data.at[id, lang_col] = prev_record + github_url
-        else:
-            leetcode_url = fetch_leetcode_url(file_path)
-            frame = pd.DataFrame([[id, leetcode_url, github_url]], columns=[data.index.name, name_col, lang_col])
-            accumulator.append(frame.set_index(data.index.name))
-
-    return accumulator
-
-
-prev_data = pd.read_csv('./docs/data.csv', index_col='ID')
-name_col, lang_col = prev_data.columns[0], prev_data.columns[1]
-
-temp_data = populate_data(prev_data)
-new_data = pd.concat([temp_data[i] for i in range(len(temp_data))]).sort_index()
-
-if not new_data.equals(prev_data):
-    update_files(new_data)
+        f.write(frames.to_markdown(colalign=("center", "center", "center")))
