2f4ac9a7c5c66cf5bad97d02fcb58ac277350ca9
Leonardo Wajda <dev.leowajda@tuta.io>
2022-12-14 12:01:13 +0100

    :art: Minor refactorings

--- tests/array/iterative/lc_0746_test.py
+++ tests/array/iterative/lc_0746_test.py
@@ -10,8 +10,8 @@ class TestSolution:
         return Solution()
 
     @pytest.mark.parametrize("cost, expected", [
-        ([10,15,20],                  15),
-        ([1,100,1,1,1,100,1,1,100,1], 6)
+        ([10, 15, 20],                         15),
+        ([1, 100, 1, 1, 1, 100, 1, 1, 100, 1], 6)
     ])
     def test_search_range(self, solution, cost, expected):
         result = solution.minCostClimbingStairs(cost)
