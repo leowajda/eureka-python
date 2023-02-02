1a7afa1797509577ae310218b4039e3f2045a9c2
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-02 12:48:11 +0100

    :art: Rewrite problem as state machine

--- src/array/iterative/lc_0309.py
+++ src/array/iterative/lc_0309.py
@@ -3,9 +3,11 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        cooldown, acquisition, profit = 0, float('-inf'), float('-inf')
 
+        prev_state = [0, float('-inf'), float('-inf')]
         for price in prices:
-            cooldown, acquisition, profit = max(cooldown, profit), max(acquisition, cooldown - price), acquisition + price
+            prev_cooldown, prev_acquisition, prev_profit = prev_state
+            prev_state = [max(prev_profit, prev_cooldown), max(prev_acquisition, prev_cooldown - price), prev_acquisition + price]
 
-        return max(profit, cooldown)
+        max_cooldown, _, max_profit = prev_state
+        return max(max_cooldown, max_profit)
