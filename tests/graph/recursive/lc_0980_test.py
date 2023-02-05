ea40ffdbe882729c0c63d6ace50fab98e6d4f37a
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-05 11:54:34 +0100

    :art: Improve readability

--- tests/graph/recursive/lc_0980_test.py
+++ tests/graph/recursive/lc_0980_test.py
@@ -10,20 +10,9 @@ class TestSolution:
         return Solution()
 
     @pytest.mark.parametrize("grid, expected", [
-        ([
-             [1, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 2, -1]
-         ], 2),
-        ([
-             [1, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 2]
-        ], 4),
-        ([
-             [0, 1],
-             [2, 0]
-        ], 0)
+        ([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, -1]], 2),
+        ([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 2]],  4),
+        ([[0, 1], [2, 0]],                            0)
     ])
     def test_uniquePathsIII(self, solution, grid, expected):
         result = solution.uniquePathsIII(grid)
